f0036f67ebe6619ce383afbcb46822de54dcae3b
jiangsongyang jiangsongyang@aecg.com.cn <jiangsongyang@aecg.com.cn>
2022-06-27 16:48:59 +0800

    [feat] : first commit

---
+++ react/node_modules/danger/distribution/platforms/gitlab/GitLabGit.d.ts
@@ -0,0 +1,3 @@
+import { GitLabDSL } from "../../dsl/GitLabDSL";
+import { GitDSL, GitJSONDSL } from "../../dsl/GitDSL";
+export declare const gitLabGitDSL: (gitlab: GitLabDSL, json: GitJSONDSL) => GitDSL;
